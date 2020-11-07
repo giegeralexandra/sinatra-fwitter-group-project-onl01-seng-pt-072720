7089943cddbef24cca3f12781dd44043a0685b07
giegeralexandra <giegeralexandra@gmail.com>
2020-11-07 13:49:46 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,94 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do 
+        if Helpers.logged_in?(session)
+            @user = User.find(session[:user_id])
+            @tweets = Tweet.all
+            erb :'tweets/index'
+        else 
+            redirect "/login"
+        end
+        #displays all tweets for logged in users 
+        #if user is not logeed in, redirect to /login
+    end
+
+    get '/tweets/new' do 
+        if Helpers.logged_in?(session)
+            erb :'tweets/new'
+        else 
+            redirect "/login"
+        end
+        #needs to be a logged in user and has to be from their self 
+        #loads create tweet form
+
+    end
+
+    post '/tweets' do 
+        if params[:content].empty?
+            redirect "/tweets/new"
+        elsif Helpers.logged_in?(session)
+            tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+            redirect "/tweets/#{tweet.id}"
+        else 
+            redirect "/login"
+        end
+        #tweet needs to be created and saved to database
+    end
+
+    get '/tweets/:id' do 
+        @tweet = Tweet.all.find(params[:id])
+        if Helpers.logged_in?(session)
+            erb :'tweets/show'
+        else 
+            redirect "/login"
+        end
+    end
+
+    get '/tweets/:id/edit' do 
+        if !Helpers.logged_in?(session)
+            redirect "/login"
+        else
+            @tweet = Tweet.find(params[:id])
+            if @tweet.user_id != session[:user_id]
+                redirect "/tweets/#{params[:id]}"
+            else
+                erb :'tweets/edit'
+            end
+        end
+        #has to be logged in and editing their own tweet
+        #loads edit form 
+        #cannot be blank content 
+    end
+
+    patch '/tweets/:id' do 
+        if !Helpers.logged_in?(session)
+            redirect "/login"
+        else
+            @tweet = Tweet.find(params[:id])
+            if @tweet.user_id != session[:user_id]
+                redirect "/tweets/#{@tweet.id}"
+            elsif params[:content] == ""
+                redirect "/tweets/#{@tweet.id}/edit"
+            else
+                @tweet.update(content: params[:content])
+                redirect "/tweets/#{@tweet.id}/edit"
+            end
+        end
+    end
+
+    delete '/tweets/:id' do 
+        @tweet = Tweet.find(params[:id])
+        if @tweet.user_id == session[:user_id]
+            @tweet.destroy 
+            redirect "/tweets"
+        else 
+            redirect "/tweets/#{@tweet.id}"
+        end
+        #has to be logged in and deleting their own tweet 
+    #form should be found on show page 
+    #no input fields just a submit button
+    end
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,68 @@
 class UsersController < ApplicationController
 
+    get '/' do 
+        erb :'users/homepage'
+    end
+
+    get '/signup' do
+        if Helpers.logged_in?(session)
+            redirect "/tweets"
+        end
+        erb :'users/signup'
+    end
+    
+    post '/signup' do 
+        if params[:username] == "" || params[:password] == "" || params[:email] == ""
+            redirect "/signup"
+        else
+            # binding.pry
+            user = User.create(username: params[:username], password: params[:password], email: params[:email])
+            session[:user_id] = user.id
+            redirect "/tweets"
+        end
+        #processess the form submission 
+        #create user and save it to database
+        #needs to log the user in and add the user_id to the sessions hash 
+        #make sure to add signup action to home page  
+    end
+
+    get '/login' do 
+        if Helpers.logged_in?(session)
+            redirect "/tweets"
+        else 
+            erb :'users/login'
+        end
+        #displays the form to login 
+    end
+
+    post '/login' do 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id]=user.id
+            redirect "/tweets"
+        else 
+            redirect "/login"
+        end
+        #submits login form and adds user_id to the sessions hash 
+    end
+
+    get '/logout' do 
+        if Helpers.logged_in?(session)
+            session.clear
+            redirect "/login"
+        else 
+            redirect "/login"
+        end
+        #clears the session hash and redirects to the /login
+    end
+
+    get '/users/:slug' do 
+        @user = User.find_by_slug(params[:slug])
+        @tweets = @user.tweets
+        erb :'users/show'
+    end
+
+
+
 
 end
